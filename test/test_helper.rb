667d1e19fedc74cdb79bb85991ee7db724a7f07b
Martin Milata <mmilata@redhat.com>
2014-07-22 15:14:34 +0200

    Take advantage of smart-proxy plugin test support
    See https://github.com/theforeman/smart-proxy/pull/183 for more
    information.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,8 @@
-path_to_smart_proxy_repo = '../smart-proxy'
-$LOAD_PATH.unshift File.join(path_to_smart_proxy_repo, 'test')
+require 'test/unit'
+require 'mocha/setup'
+require 'rack/test'
 
-# create log directory in our (not smart-proxy) repo
-logdir = File.join(File.dirname(__FILE__), '..', 'logs')
-FileUtils.mkdir_p(logdir) unless File.exists?(logdir)
+require 'smart_proxy_for_testing'
 
-require 'test_helper'
-
-APP_ROOT = File.join(File.dirname(__FILE__), '..')
+# create log directory in our (not smart-proxy) directory
+FileUtils.mkdir_p File.dirname(Proxy::SETTINGS.log_file)
