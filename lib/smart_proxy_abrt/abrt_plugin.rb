48ff79ee6c0e270d900f47af0cb3b67788878495
Martin Milata <mmilata@redhat.com>
2014-09-16 17:45:54 +0200

    Check for presence of SSL certs at startup

--- lib/smart_proxy_abrt/abrt_plugin.rb
+++ lib/smart_proxy_abrt/abrt_plugin.rb
@@ -9,5 +9,20 @@ module AbrtProxy
     default_settings :spooldir => "/var/spool/foreman-proxy-abrt",
                      :aggregate_reports => false,
                      :server_ssl_noverify => false
+
+    after_activation do
+      if settings.server_url && !settings.server_url.to_s.empty?
+        check_file(:server_ssl_cert)
+        check_file(:server_ssl_key)
+      end
+    end
+
+    def check_file(conf_sym)
+      path = settings.send(conf_sym)
+      if path && !path.to_s.empty? && !File.exist?(path)
+        logger.error "Cannot find #{conf_sym} file #{path}"
+      end
+    end
+    private :check_file
   end
 end
